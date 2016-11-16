578dcc08d83b0ccf74734090a6e556e16dee5ca1
1w2w3y <1w2w3y@gmail.com>
2016-11-16 00:33:18 -0800

    - add packing

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack'),
+    path = require('path'),
+    yargs = require('yargs');
+
+var libraryName = 'qrcode-generator-ts',
+    plugins = [],
+    outputFile;
+
+if (yargs.argv.p) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+    outputFile = libraryName + '.min.js';
+} else {
+    outputFile = libraryName + '.js';
+}
+
+var config = {
+    entry: [
+        __dirname + '/ts/index.ts'
+    ],
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            { test: /\.tsx?$/, loader: 'ts', exclude: /node_modules/ }
+        ]
+    },
+    resolve: {
+        root: path.resolve('./ts'),
+        extensions: ['', '.js', '.ts', '.jsx', '.tsx']
+    },
+    plugins: plugins,
+
+    // Individual Plugin Options
+    tslint: {
+        emitErrors: true,
+        failOnHint: true
+    }
+};
+
+module.exports = config;
